cf6e32cb593a247aa43b36a98bb2f1102de9f284
qqingzheng <2533221180@qq.com>
2024-11-20 22:17:55 +0800

    remove dir

--- causalvideovae/model/adapters/config.py
+++ causalvideovae/model/adapters/config.py
@@ -1,3 +1,3 @@
 import os
 
-adapter_path = "/storage/lcm/WF-VAE_paper/causalvideovae/model/adapters" # os.path.dirname(os.path.abspath(__file__))
+adapter_path = "Anonymous" # os.path.dirname(os.path.abspath(__file__))
--- scripts/merge_encoder_end_decoder.py
+++ scripts/merge_encoder_end_decoder.py
@@ -1,8 +1,8 @@
 import torch
 
-encoder = "/storage/lcm/WF-VAE_paper/results/wfvae-16dim-large/merge.ckpt"
-decoder = "/storage/lcm/WF-VAE_paper/results/FORMAL_16DIM_L_ont_resume_dic_onlydecoder-128_encode_128_decode-lr1.00e-05-bs1-rs256-sr5-fr37/checkpoint-1005000.ckpt"
-output = "/storage/lcm/WF-VAE_paper/results/wfvae-16dim-large/merge2.ckpt"
+encoder = "Anonymous"
+decoder = "Anonymous"
+output = "Anonymous"
 
 encoder_ckpt = torch.load(encoder, map_location="cpu")
 decoder_ckpt = torch.load(decoder, map_location="cpu")
